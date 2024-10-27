833811e44a55465eb6fc0eedcaa2543f605cd7a4
MuhammadYaafay <muhammad.yaffay@gmail.com>
2024-10-28 00:49:01 +0500

    dist

--- main.cpp
+++ main.cpp
@@ -2,36 +2,36 @@
 using namespace std;
 
 class InfectionSpread{
-
+    //yaafay
 };
 
 class DynamicIntervention{
-
+    //adil
 };
 
 class InfectionTrendsAndStatistics{
-
+    //ali
 };
 
 class CustomizableInfectionRates{
-
+    //ali
 };
 
 class RecoverySimulation{
-
+    //adil
 };
 
 class AddandRemoveRegions{
-
+    //ali
 };
 
 class OptimizedRouteandPolicySuggestions{
-
+// yaafay
 };
 
 
 class AdjustmentsBasedonNewData{
-
+    //adil
 };
 
 
